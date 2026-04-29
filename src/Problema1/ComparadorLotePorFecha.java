d9a5d5a8bbc5ffc4b7c6085ca840b9b2b97cb9d8
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-29 17:45:14 -0600

    Update ComparadorLotePorFecha.java

--- src/Problema1/ComparadorLotePorFecha.java
+++ src/Problema1/ComparadorLotePorFecha.java
@@ -1,4 +1,12 @@
 package Problema1;
 
-public class ComparadorLotePorFecha {
+import java.util.Comparator;
+
+
+public class ComparadorLotePorFecha implements Comparator<Lote> {
+
+    @Override
+    public int compare(Lote l1, Lote l2) {
+        return l1.getFechaFabricacion().compareTo(l2.getFechaFabricacion());
+    }
 }
