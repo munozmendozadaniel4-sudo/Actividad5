1f10445a95065e4f6386237c2b8483101d4bf88b
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-29 17:45:35 -0600

    Update ComparadorPrendaPorModelo.java

--- src/Problema1/ComparadorPrendaPorModelo.java
+++ src/Problema1/ComparadorPrendaPorModelo.java
@@ -1,4 +1,10 @@
 package Problema1;
+import java.util.Comparator;
 
-public class ComparadorPrendaPorModelo {
+public class ComparadorPrendaPorModelo implements Comparator<Prenda> {
+
+    @Override
+    public int compare(Prenda p1, Prenda p2) {
+        return p1.getModelo().compareTo(p2.getModelo());
+    }
 }
