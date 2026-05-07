2496186d6f3e13eed7e5486e86c51a3d632d2539
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-05-06 23:33:27 -0600

    Update PeriodoVisible.java

--- src/Problema3/PeriodoVisible.java
+++ src/Problema3/PeriodoVisible.java
@@ -1,4 +1,49 @@
 package Problema3;
 
-public class PeriodoVisible {
+import java.util.List;
+import java.util.Objects;
+
+public class PeriodoVisible implements Comparable<PeriodoVisible> {
+
+    private String periodo;
+
+    public PeriodoVisible(String periodo) throws MesInvalidoException {
+        validarMeses(periodo);
+        this.periodo = periodo;
+    }
+
+    private void validarMeses(String periodo) throws MesInvalidoException {
+        List<String> mesesValidos = MesInvalidoException.getMesesValidos();
+        String[] partes = periodo.split(" - ");
+        for (String parte : partes) {
+            String mes = parte.trim();
+            if (!mesesValidos.contains(mes)) {
+                throw new MesInvalidoException(mes);
+            }
+        }
+    }
+
+    public String getPeriodo() { return periodo; }
+
+    @Override
+    public String toString() {
+        return "PeriodoVisible{periodoObservado='" + periodo + "'}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PeriodoVisible that = (PeriodoVisible) o;
+        return Objects.equals(periodo, that.periodo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(periodo);
+    }
+
+    @Override
+    public int compareTo(PeriodoVisible otro) {
+        return this.periodo.compareTo(otro.periodo);
+    }
 }
