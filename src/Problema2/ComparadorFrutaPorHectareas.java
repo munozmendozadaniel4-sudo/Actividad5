792178d277a0cb5749982126ce3730d381023b99
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-05-06 23:30:12 -0600

    Update ComparadorFrutaPorHectareas.java

--- src/Problema2/ComparadorFrutaPorHectareas.java
+++ src/Problema2/ComparadorFrutaPorHectareas.java
@@ -1,4 +1,10 @@
 package Problema2;
+import java.util.Comparator;
 
-public class ComparadorFrutaPorHectareas {
+public class ComparadorFrutaPorHectareas implements Comparator<Fruta> {
+
+    @Override
+    public int compare(Fruta f1, Fruta f2) {
+        return Double.compare(f1.getHectareas(), f2.getHectareas());
+    }
 }
