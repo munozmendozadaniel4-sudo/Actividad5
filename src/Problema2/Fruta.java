6a4dbf471f86ec305e25327e5e42ed1640572bb6
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-05-06 23:31:57 -0600

    Update Fruta.java

--- src/Problema2/Fruta.java
+++ src/Problema2/Fruta.java
@@ -1,72 +1,96 @@
 package Problema2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+public class Fruta implements Comparable<Fruta> {
 
-public class PeriodoDeCosecha implements Comparable<PeriodoDeCosecha> {
+    private static final double MIN_HECTAREAS = 0.5;
+    private static final double MAX_HECTAREAS_ABSOLUTO = 10.0;
+    private static final double MIN_PRECIO_FACTOR = 1.05;
+    private static final double MAX_PRECIO_FACTOR = 1.30;
 
-    private static final double MIN_TONELADAS = 0.5;
-    private static final double MAX_TONELADAS = 200.0;
+    private String nombreFruta;
+    private double hectareas;
+    private double costoProduccionTon;
+    private double precioVentaTon;
+    private List<PeriodoDeCosecha> periodos;
+    private double limiteHectareas;
 
-    private String nombrePeriodo;
-    private double cantidadToneladas; // toneladas por hectarea
-    private Fruta fruta;
+    public Fruta(String nombreFruta, double hectareas, double costoProduccionTon,
+                 double precioVentaTon, double limiteHectareas)
+            throws ExtensionTerrenoInvalidaException, PrecioVentaInvalidoException {
 
-    public PeriodoDeCosecha(String nombrePeriodo, double cantidadToneladas, Fruta fruta)
-            throws CantidadCosechaInvalidaException {
 
-        validarCantidad(cantidadToneladas);
+        double limiteReal = Math.min(limiteHectareas, MAX_HECTAREAS_ABSOLUTO);
 
-        this.nombrePeriodo = nombrePeriodo;
-        this.cantidadToneladas = cantidadToneladas;
-        this.fruta = fruta;
+        validarHectareas(hectareas, limiteReal);
+        validarPrecioVenta(precioVentaTon, costoProduccionTon);
+
+        this.nombreFruta = nombreFruta;
+        this.hectareas = hectareas;
+        this.costoProduccionTon = costoProduccionTon;
+        this.precioVentaTon = precioVentaTon;
+        this.limiteHectareas = limiteReal;
+        this.periodos = new ArrayList<>();
     }
 
-    private void validarCantidad(double cantidad) throws CantidadCosechaInvalidaException {
-        if (cantidad < MIN_TONELADAS || cantidad > MAX_TONELADAS) {
-            throw new CantidadCosechaInvalidaException(cantidad);
+    private void validarHectareas(double hectareas, double limite) throws ExtensionTerrenoInvalidaException {
+        if (hectareas < MIN_HECTAREAS || hectareas > limite) {
+            throw new ExtensionTerrenoInvalidaException(hectareas, limite);
         }
     }
-    
-    public double produccionTotal() {
-        return cantidadToneladas * fruta.getHectareas();
-    }
-    
-    public double costoProduccion() {
-        return produccionTotal() * fruta.getCostoProduccionTon();
-    }
-    
-    public double gananciasEstimadas() {
-        return produccionTotal() * fruta.getPrecioVentaTon() - costoProduccion();
+
+    private void validarPrecioVenta(double precio, double costo) throws PrecioVentaInvalidoException {
+        double min = costo * MIN_PRECIO_FACTOR;
+        double max = costo * MAX_PRECIO_FACTOR;
+        if (precio < min || precio > max) {
+            throw new PrecioVentaInvalidoException(precio, costo);
+        }
     }
 
-    public String getNombrePeriodo() { return nombrePeriodo; }
-    public double getCantidadToneladas() { return cantidadToneladas; }
-    public Fruta getFruta() { return fruta; }
+    public void agregarPeriodo(PeriodoDeCosecha periodo) throws TiempoCosechaInvalidoException {
+        if (periodos.size() >= 6) {
+            throw new TiempoCosechaInvalidoException(periodos.size() + 1);
+        }
+        periodos.add(periodo);
+    }
+
+    public void eliminarPeriodo(PeriodoDeCosecha periodo) {
+        periodos.remove(periodo);
+    }
+
+    public String getNombreFruta() { return nombreFruta; }
+    public double getHectareas() { return hectareas; }
+    public double getCostoProduccionTon() { return costoProduccionTon; }
+    public double getPrecioVentaTon() { return precioVentaTon; }
+    public List<PeriodoDeCosecha> getPeriodos() { return periodos; }
 
     @Override
     public String toString() {
-        return "PeriodoDeCosecha{" +
-                "periodo='" + nombrePeriodo + '\'' +
-                ", produccionEstimada=" + cantidadToneladas + " ton/ha" +
-                '}';
+        return "Fruta{nombre='" + nombreFruta + '\'' +
+                ", hectareas=" + hectareas +
+                ", costoProduccionTon=" + costoProduccionTon +
+                ", precioVentaTon=" + precioVentaTon + '}';
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        PeriodoDeCosecha that = (PeriodoDeCosecha) o;
-        return Double.compare(cantidadToneladas, that.cantidadToneladas) == 0 &&
-                Objects.equals(nombrePeriodo, that.nombrePeriodo) &&
-                Objects.equals(fruta, that.fruta);
+        Fruta fruta = (Fruta) o;
+        return Double.compare(hectareas, fruta.hectareas) == 0 &&
+                Double.compare(costoProduccionTon, fruta.costoProduccionTon) == 0 &&
+                Double.compare(precioVentaTon, fruta.precioVentaTon) == 0 &&
+                Objects.equals(nombreFruta, fruta.nombreFruta);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nombrePeriodo, cantidadToneladas, fruta);
+        return Objects.hash(nombreFruta, hectareas, costoProduccionTon, precioVentaTon);
     }
 
     @Override
-    public int compareTo(PeriodoDeCosecha otro) {
-        return this.nombrePeriodo.compareTo(otro.nombrePeriodo);
+    public int compareTo(Fruta otra) {
+        return this.nombreFruta.compareTo(otra.nombreFruta);
     }
 }
